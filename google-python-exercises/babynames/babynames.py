a4a1c1d4f66443ac6f4aafef25c84969939f71ea
Misha Leong <mishaleong@gmail.com>
2019-04-05 19:11:53 -0700

    just starting this project

--- google-python-exercises/babynames/babynames.py
+++ google-python-exercises/babynames/babynames.py
@@ -40,8 +40,12 @@ def extract_names(filename):
   followed by the name-rank strings in alphabetical order.
   ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
   """
-  # +++your code here+++
-  return
+  input_file = open(filename, 'r')
+  for line in input_file:
+      # somethong Here
+
+  input_file.close()  # Not strictly required, but good form.
+  return name_list
 
 
 def main():
@@ -63,6 +67,6 @@ def main():
   # +++your code here+++
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
-  
+
 if __name__ == '__main__':
   main()
